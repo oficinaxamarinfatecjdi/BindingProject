454fab347ae0678bc0a1dc180a22406307d67b3a
Rodrigo de Oliveira Soares <rodrigo.o.soares@hotmail.com>
2018-09-05 12:33:16 -0300

    Código completo

---
+++ BindingProject/BindingProject/BindingProject/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BindingProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BindingXaml_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PorXaml.BindingXamlPage());
+        }
+
+        private void BindingCSharp_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PorCSharp.BindingCSharpPage());
+        }
+    }
+}
---
+++ BindingProject/BindingProject/BindingProject/PorCSharp/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BindingProject.PorCSharp
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; } = "Carlos";
+        public int Idade { get; set; } = 19;
+    }
+}
